e87ec9f4f2a85d0ee1177dc81ac004eb418caead
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-13 21:42:41 +0530

    Spring Aop Project to Learn the Aop Annotations

--- 01_SpringbootAop/src/main/java/com/pavan/aspects/TransactionManagment.java
+++ 01_SpringbootAop/src/main/java/com/pavan/aspects/TransactionManagment.java
@@ -1,5 +1,22 @@
 package com.pavan.aspects;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
 public class TransactionManagment {
 
+	
+	
+	public void begingTansaction()
+	{
+		
+	}
+	public void endTansaction()
+	{
+		
+	}
+	
+	
 }
--- 01_SpringbootAop/src/main/java/com/pavan/dao/EmployeeDao.java
+++ 01_SpringbootAop/src/main/java/com/pavan/dao/EmployeeDao.java
@@ -2,4 +2,19 @@ package com.pavan.dao;
 
 public class EmployeeDao {
 
+	public void saveEmployee()
+	{
+		System.out.println("EmployeeDao.saveEmployee()");
+	}
+	
+	public void getEmployee()
+	{
+		System.out.println("EmployeeDao.getEmployee()");
+	}
+	
+	public String deleteEmployee()
+	{
+		return "Employee deleted Successfully ::::";
+	}
+	
 }
