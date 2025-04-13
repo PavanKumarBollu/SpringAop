50ace00f32154456911f40e24e090867a719dade
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-13 21:49:19 +0530

    Spring Aop Project to Learn the Aop Annotations

--- 01_SpringbootAop/src/main/java/com/pavan/aspects/TransactionManagment.java
+++ 01_SpringbootAop/src/main/java/com/pavan/aspects/TransactionManagment.java
@@ -1,22 +1,28 @@
 package com.pavan.aspects;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class TransactionManagment {
 
-	
-	
-	public void begingTansaction()
-	{
+	@Pointcut(value = "public void com.pavan.dao.EmployeeDao.saveEmployee()")
+	public void operation() {
+
+	}
+
+	@Before(value = "operation()")// JoinPoint
+	public void begingTansaction() {
 		System.out.println("Transactionhas Begen");
 	}
-	public void commitTansaction()
-	{
+
+	@After(value="operation()")
+	public void commitTansaction() {
 		System.out.println("Transaction Commited ...");
 	}
-	
-	
+
 }
