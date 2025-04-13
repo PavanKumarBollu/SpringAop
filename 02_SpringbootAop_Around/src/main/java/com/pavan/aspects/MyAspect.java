1b66ab622a5d03187ea3dda480510cac5200527c
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-13 22:35:09 +0530

    Spring Aop Project to Learn the Aop Annotations Around Annotation

--- 02_SpringbootAop_Around/src/main/java/com/pavan/aspects/MyAspect.java
+++ 02_SpringbootAop_Around/src/main/java/com/pavan/aspects/MyAspect.java
@@ -1,5 +1,21 @@
 package com.pavan.aspects;
 
-public class MyAspect {
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
+@Aspect
+@Component
+public class MyAspect {
+	
+	// pointcut
+	@Pointcut(value = "execution(public int com.pavan.dao.EmployeeDao.saveEmployee())" )
+	public void save() {}
+	
+	@Around(value = "save()")
+	public void around()
+	{
+		
+	}
 }
