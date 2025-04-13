328f879a5692755a94e137fb70d4a01012e59629
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-13 23:17:19 +0530

    Spring Aop Project to Learn the Aop and working with the custom annotation to support the AOP

--- 03_SpringbootAop_CustomAnnotationToSupportAOP/src/main/java/com/pavan/aspects/StudentAspect.java
+++ 03_SpringbootAop_CustomAnnotationToSupportAOP/src/main/java/com/pavan/aspects/StudentAspect.java
@@ -1,18 +1,28 @@
 package com.pavan.aspects;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class StudentAspect {
 	
+	@Pointcut(value = "@annotation(com.pavan.annotation.MyAnnotationAspect)")
+	public void save() {}
 	
 	
-	@Before
+	@Before(value = "save()")
 	public void before()
 	{
-		System.out.println("Transaction has Begin");
+		System.out.println("Transaction has Begin ..... ");
+	}
+	
+	@After(value="save()")
+	public void after()
+	{
+		System.out.println("Tansaction commited ......");
 	}
 }
