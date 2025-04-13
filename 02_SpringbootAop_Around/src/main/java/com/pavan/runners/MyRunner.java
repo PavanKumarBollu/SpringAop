808117a2348a61a659921c57257a9c21d6d69ef6
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-13 22:33:51 +0530

    Spring Aop Project to Learn the Aop Annotations Around Annotation

--- 02_SpringbootAop_Around/src/main/java/com/pavan/runners/MyRunner.java
+++ 02_SpringbootAop_Around/src/main/java/com/pavan/runners/MyRunner.java
@@ -1,15 +1,19 @@
 package com.pavan.runners;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.pavan.dao.EmployeeDao;
+
 @Component
 public class MyRunner implements CommandLineRunner {
+	@Autowired
+	private EmployeeDao dao;
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		
+		dao.saveEmployee();
 	}
 
 	
