7f96985f6f4725feb64a7bc9ddd13889bb0e1946
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-13 23:30:16 +0530

    Springboot Runner concept

--- 04_SpringbootRunners/src/main/java/com/pavan/runners/ApplicationRunnerImpl.java
+++ 04_SpringbootRunners/src/main/java/com/pavan/runners/ApplicationRunnerImpl.java
@@ -1,5 +1,7 @@
 package com.pavan.runners;
 
+import java.util.Arrays;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
@@ -8,6 +10,7 @@ public class ApplicationRunnerImpl implements ApplicationRunner {
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		System.out.println("ApplicationRunnerImpl.run()");
+		System.out.println("arguments are :: " + Arrays.asList(args));
 	}
 
 }
--- 04_SpringbootRunners/src/main/java/com/pavan/runners/CommandLineRunnerImpl.java
+++ 04_SpringbootRunners/src/main/java/com/pavan/runners/CommandLineRunnerImpl.java
@@ -1,5 +1,7 @@
 package com.pavan.runners;
 
+import java.util.Arrays;
+
 import org.springframework.boot.CommandLineRunner;
 
 public class CommandLineRunnerImpl implements CommandLineRunner {
@@ -7,6 +9,10 @@ public class CommandLineRunnerImpl implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("CommandLineRunnerImpl.run()");
+		System.out.println("args[0] :: "+ args[0]);
+		System.out.println("Arguments are :: " + Arrays.asList(args));
+		System.out.println();
+		
 	}
 
 }
